cb08cfb3a526f0f46447d8da8adc1ee8d3a07f01
shiheng <Xheng4@hotmail.com>
2017-03-16 09:00:17 +0800

    添加首页数据和间距

--- app/src/main/java/com/example/xheng/welfaresociety/ui/adapter/NewGoodsAdapter.java
+++ app/src/main/java/com/example/xheng/welfaresociety/ui/adapter/NewGoodsAdapter.java
@@ -1,26 +1,84 @@
 package com.example.xheng.welfaresociety.ui.adapter;
 
+import android.content.Context;
 import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.RecyclerView.ViewHolder;
+import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.example.xheng.welfaresociety.R;
+import com.example.xheng.welfaresociety.model.bean.NewGoodsBean;
+import com.example.xheng.welfaresociety.model.net.INewGoodsModel;
+import com.example.xheng.welfaresociety.model.net.NewGoodsModel;
+import com.example.xheng.welfaresociety.model.utils.ImageLoader;
+
+import java.util.ArrayList;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
 
 /**
  * Created by xheng on 2017/3/15.
  */
 
 public class NewGoodsAdapter extends RecyclerView.Adapter<RecyclerView.ViewHolder> {
+    Context context;
+    ArrayList<NewGoodsBean> mList;
+    INewGoodsModel model;
+
+    public NewGoodsAdapter(Context context, ArrayList<NewGoodsBean> mList) {
+        this.context = context;
+        this.mList = mList;
+        model = new NewGoodsModel();
+    }
 
     @Override
     public RecyclerView.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
-        return null;
+        ViewHolder viewHolder = new GoodsViewHolder(View.inflate(context, R.layout.item_goods, null));
+        return viewHolder;
     }
 
     @Override
     public void onBindViewHolder(RecyclerView.ViewHolder holder, int position) {
-
+        GoodsViewHolder viewHolder = (GoodsViewHolder) holder;
+        NewGoodsBean bean = mList.get(position);
+        viewHolder.mTvName.setText(bean.getGoodsName());
+        viewHolder.mTvPrice.setText(bean.getShopPrice());
+//        ImageLoader.downloadImg(context, viewHolder.mIvGoods, bean.getGoodsThumb());
     }
 
     @Override
     public int getItemCount() {
-        return 0;
+        return mList != null ? mList.size() : 0;
+    }
+
+    class GoodsViewHolder extends ViewHolder{
+        @BindView(R.id.iv_goods)
+        ImageView mIvGoods;
+        @BindView(R.id.tv_name)
+        TextView mTvName;
+        @BindView(R.id.tv_price)
+        TextView mTvPrice;
+
+        GoodsViewHolder(View view) {
+            super(view);
+            ButterKnife.bind(this, view);
+        }
     }
 }
+
+/*
+class GoodsViewHolder extends RecyclerView.ViewHolder {
+    TextView tvName, tvPrice;
+    ImageView imageView;
+
+    public GoodsViewHolder(View itemView) {
+        super(itemView);
+        tvName = (TextView) itemView.findViewById(R.id.tv_name);
+        tvPrice = (TextView) itemView.findViewById(R.id.tv_price);
+        imageView = (ImageView) itemView.findViewById(R.id.iv_goods);
+    }
+}
+*/
--- app/src/main/java/com/example/xheng/welfaresociety/ui/fragment/NewGoodsFragment.java
+++ app/src/main/java/com/example/xheng/welfaresociety/ui/fragment/NewGoodsFragment.java
@@ -2,17 +2,52 @@ package com.example.xheng.welfaresociety.ui.fragment;
 
 
 import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v4.widget.SwipeRefreshLayout;
+import android.support.v7.widget.GridLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import com.example.xheng.welfaresociety.R;
+import com.example.xheng.welfaresociety.application.I;
+import com.example.xheng.welfaresociety.model.bean.NewGoodsBean;
+import com.example.xheng.welfaresociety.model.net.INewGoodsModel;
+import com.example.xheng.welfaresociety.model.net.NewGoodsModel;
+import com.example.xheng.welfaresociety.model.net.OnCompleteListener;
+import com.example.xheng.welfaresociety.model.utils.L;
+import com.example.xheng.welfaresociety.model.utils.ResultUtils;
+import com.example.xheng.welfaresociety.ui.adapter.NewGoodsAdapter;
+import com.example.xheng.welfaresociety.ui.widget.SpaceItemDecoration;
+
+import java.util.ArrayList;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.Unbinder;
 
 /**
  * A simple {@link Fragment} subclass.
  */
 public class NewGoodsFragment extends Fragment {
+    private static final String TAG = NewGoodsFragment.class.getSimpleName();
+
+    @BindView(R.id.tv_refresh)
+    TextView mTvRefresh;
+    @BindView(R.id.recycler_view)
+    RecyclerView mRecyclerView;
+    @BindView(R.id.srl)
+    SwipeRefreshLayout mSwipeRefresh;
+    Unbinder bind;
+
+    INewGoodsModel newGoodsModel;
+    int mPageID = 1;
+    GridLayoutManager mManager;
+    NewGoodsAdapter mAdapter;
+    ArrayList<NewGoodsBean> mList;
 
 
     public NewGoodsFragment() {
@@ -24,8 +59,55 @@ public class NewGoodsFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_new_goods, container, false);
-        // Inflate the layout for this fragment
+        bind = ButterKnife.bind(this, view);
+        initView();
+//        ButterKnife.bind(this, view);
         return view;
     }
 
+    private void initView() {
+        mManager = new GridLayoutManager(getActivity(), I.COLUM_NUM);
+        mRecyclerView.setLayoutManager(mManager);
+        mRecyclerView.setHasFixedSize(true);//自动修正大小
+        mList = new ArrayList<>();
+        mAdapter = new NewGoodsAdapter(getActivity(), mList);
+        mRecyclerView.setAdapter(mAdapter);
+        mRecyclerView.addItemDecoration(new SpaceItemDecoration(12));
+    }
+
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+        newGoodsModel = new NewGoodsModel();
+        initData();
+    }
+
+    private void initData() {
+        newGoodsModel.loadData(getActivity(), mPageID, new OnCompleteListener<NewGoodsBean[]>() {
+            @Override
+            public void onSuccess(NewGoodsBean[] result) {
+                L.e(TAG, "onSuccess() result:" + result);
+                if (result != null && result.length > 0) {
+                    ArrayList<NewGoodsBean> list = ResultUtils.array2List(result);
+                    mList.clear();
+                    mList.addAll(list);
+                    mAdapter.notifyDataSetChanged();
+                }
+            }
+
+            @Override
+            public void onError(String error) {
+                L.e(TAG, "onSuccess() error:" + error);
+
+            }
+        });
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        if (bind != null) {
+            bind.unbind();
+        }
+    }
 }
